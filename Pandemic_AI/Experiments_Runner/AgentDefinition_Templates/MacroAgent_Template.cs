658034ac25f0f8d522df14d820d0a92cb5abfc1b
konstantinos sfikas <kon.sfik@gmail.com>
2020-05-31 09:33:23 +0300

    added a template for a macro actions - based agent

---
+++ Pandemic_AI/Experiments_Runner/AgentDefinition_Templates/MacroAgent_Template.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pandemic_AI_Framework;
+
+namespace Experiments_Runner
+{
+    /// <summary>
+    /// A template for developing an agent that operates within the 
+    /// macro action - based representation of the game state.
+    /// </summary>
+    public class MacroAgent_Template : PD_AI_Macro_Agent_Base
+    {
+        /// <summary>
+        /// Returns a description of the agent
+        /// This is optional and has nothing to do with the agent's functionality.
+        /// </summary>
+        /// <returns></returns>
+        public override string GetDescription()
+        {
+            return this.GetType().Name;
+        }
+
+        protected override PD_MacroAction Discarding_AfterDrawing_Behaviour(PD_Game game, PD_AI_PathFinder pathFinder)
+        {
+            var allMacros = game.CurrentAvailableMacros;
+            return allMacros.GetOneRandom();
+        }
+
+        protected override PD_MacroAction Discarding_DuringMainPlayerActions_Behaviour(PD_Game game, PD_AI_PathFinder pathFinder)
+        {
+            var allMacros = game.CurrentAvailableMacros;
+            return allMacros.GetOneRandom();
+        }
+
+        protected override PD_MacroAction MainPlayerActions_Behaviour(PD_Game game, PD_AI_PathFinder pathFinder)
+        {
+            var allMacros = game.CurrentAvailableMacros;
+            return allMacros.GetOneRandom();
+        }
+    }
+}
